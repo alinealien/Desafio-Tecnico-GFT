222d561955f33b16f17316e169459dae24e03702
alinealien <alinebdantunes@gmail.com>
2022-05-24 15:16:58 -0300

    Corrigindo erros nos codigos

--- src/Main.java
+++ src/Main.java
@@ -9,38 +9,38 @@ public class Main {
 
     public static void main(String[] args){
 
-        NumeroReverso.numeroReverso();
+        // TESTES
+
+        //NumeroReverso.numeroReverso();
+
+        /*Macaco macaco1 = new Macaco();
+        Macaco macaco2 = new Macaco();
+        macaco1.comer("banana");
+        macaco1.comer("banana");
+        macaco1.comer("banana");
+        macaco2.comer("pêra");
+        macaco2.comer("uva");
+        macaco2.comer("maçã");
+        System.out.println("------");
+        macaco1.digerir();
+        macaco1.digerir();
+        macaco1.digerir();
+        macaco2.digerir();
+        macaco2.digerir();
+        macaco2.digerir();
+        macaco2.digerir();*/
+
+        /*Vendedor vendedor = new Vendedor("Camila Vendedora", 123456789L, 10d, 5);
+        System.out.println(vendedor.calcularSalario());
+
+        Consultor consultor = new Consultor("Consultor", 789456123L, 100.5d, 10);
+        System.out.println(consultor.calcularSalario());*/
 
 
 
     }
 
-    // TESTES
 
-   //NumeroReverso.numeroReverso(); OK
-
-    /*Macaco macaco1 = new Macaco();
-    Macaco macaco2 = new Macaco();
-    macaco1.comer("banana");
-    macaco1.comer("banana");
-    macaco1.comer("banana");
-    macaco2.comer("pêra");
-    macaco2.comer("uva");
-    macaco2.comer("maçã");
-    System.out.println("------");
-    macaco1.digerir();
-    macaco1.digerir();
-    macaco1.digerir();
-    macaco2.digerir();
-    macaco2.digerir();
-    macaco2.digerir();
-    macaco2.digerir();*/
-
-    /*Vendedor vendedor = new Vendedor("Camila Vendedora", 123456789L, 10d, 5);
-    System.out.println(vendedor.calcularSalario());
-
-    Consultor consultor = new Consultor("Consultor", 789456123L, 100.5d, 10);
-    System.out.println(consultor.calcularSalario());*/
 
 
 }
